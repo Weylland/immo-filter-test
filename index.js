69065960543cba2a3a50bbf599b0bab1f314a422
Weylland <samiernicolas62@gmail.com>
2022-02-13 18:05:28 +0100

    Update index.js
    J'ai commenté le code

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const btns = btnContainer.getElementsByClassName("btn");
 const x = document.getElementsByClassName("filterDiv");
 const current = document.getElementsByClassName("active");
 
+console.log(x);
+console.log(x[0]);
+
 // On définie le filtre de base au chargement de la page
 
 let clickedBtn = "all";
@@ -48,7 +51,7 @@ colorsBtn.addEventListener("click", () => {
 // Fonction de selection du filtre
 
 function filterSelection(c) {
-  // si c donc la variable clickedBtn est égale à "all" clickedBtn = ""
+  // si c, donc la variable clickedBtn est égale à "all" clickedBtn = ""
 
   if (c == "all") c = "";
 
@@ -57,18 +60,46 @@ function filterSelection(c) {
   // au nombre d'élément du tableau on repasse dans la boucle
 
   for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    // on déclanche la fonction pour retirer la classe "show"
+    removeClass(x[i], "show");
+
+    // indexOf() renvoie l'index d'un élément dans un tableau si cette élément n'éxiste pas cela renvoie -1
+    // <div class="filterDiv cars">BMW</div> est le premier élément du tableau
+    // En premier on vérifie que l'élément éxiste bien dans le tableau grace à > -1
+    // si (x[<div class="filterDiv cars">BMW</div>] contient le nom de classe "cars" ) ajoute la classe "show"
+    // Comme "cars" éxiste bien indexOf() va renvoyer "1" car cars est placé en deuxième position
+
+    // Autre exemple pour x[<div class="filterDiv colors fruits">Orange</div>] ici on ne retrouve pas "cars" ce qui renvera -1
+    // la fonction addClass ne sera donc pas exécuté
+
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
   }
 }
 
 // Fonction d'ajout de classe
 
-function w3AddClass(element, name) {
+function addClass(element, name) {
+  // on initialise trois variable
+  // arr1 et arr2 vont contenir des tableaux
   let i, arr1, arr2;
+
+  // ici element est égale à x[i] donc element est égale à x[<div class="filterDiv cars">BMW</div>] por l'index 0
+  // arr1 element.className va contenir les noms de classes de la div selectionné sous forme de chaine de charactère
+  // split va découper cette chaine en un tableau split va découper la chaine au endroit ou il y a un espace grace au guillemet dans les parenthèses
+  // pareil pour arr2
+
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
+
+  console.log(arr2);
+
+  // arr2 à toujours une length de 1 car il contient toujours 1 élément qui est "show" la boucle ne s'execute qu'une fois
+
   for (i = 0; i < arr2.length; i++) {
+    // arr2[i] renvoie show
+    // arr1.indexOf(arr2[i]) revient à vérifier arr1.indexOf("show")
+    // si show n'existe pas dans arr1
+
     if (arr1.indexOf(arr2[i]) == -1) {
       element.className += " " + arr2[i];
     }
@@ -77,10 +108,12 @@ function w3AddClass(element, name) {
 
 // Fonction de retrait de classe
 
-function w3RemoveClass(element, name) {
+function removeClass(element, name) {
   let i, arr1, arr2;
+
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
+
   for (i = 0; i < arr2.length; i++) {
     while (arr1.indexOf(arr2[i]) > -1) {
       arr1.splice(arr1.indexOf(arr2[i]), 1);
